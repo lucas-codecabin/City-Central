0bae4ee5f72f0bc6cd704065accbb59a1a8caca8
lucas-codecabin <lucas@codecabin.com.au>
2025-04-25 13:19:10 +1000

    Create users page

--- components/GridUsers.vue
+++ components/GridUsers.vue
@@ -1,7 +1,27 @@
 <script setup>
 const supabase = useSupabaseClient();
+
+const { data, error } = await useAsyncData("user_with_role", async () => {
+  const { data } = await supabase.from("user_with_role").select();
+  return data;
+});
 </script>
 
 <template>
-  <div class="grid grid-cols-1 md:grid-cols-5 p-8 gap-8">Users</div>
+  <div class="grid grid-cols-1 md:grid-cols-5 p-8 gap-8">
+    <div class="card col-span-5">
+      <DataTable
+        :value="data"
+        tableStyle="width: 100%"
+        stripedRows
+        removableSort
+        sortField="first_name"
+        :sortOrder="1"
+      >
+        <Column field="first_name" header="User" sortable></Column>
+        <Column field="email" header="Email" sortable></Column>
+        <Column field="role_title" header="Role" sortable></Column>
+      </DataTable>
+    </div>
+  </div>
 </template>
