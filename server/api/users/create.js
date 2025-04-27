3e54cc5703afcd0cf5c920f32f382ecb41db1a14
lucas-codecabin <lucas@codecabin.com.au>
2025-04-27 17:24:40 +1000

    Update api/users/create to return new auth user Id

--- server/api/users/create.js
+++ server/api/users/create.js
@@ -19,12 +19,27 @@ export default defineEventHandler(async (event) => {
       email_confirm: true,
     });
 
+    console.log("DATA:", data);
+    console.log("ERROR:", error);
+
     if (error) {
       console.error("Supabase admin error", error);
+      throw createError({ statusCode: 400, statusMessage: error.message });
+    }
+
+    if (!data?.user?.id) {
+      throw createError({
+        statusCode: 500,
+        statusMessage: "User ID missing after signup",
+      });
     }
 
     return { id: data.user.id };
   } catch (error) {
     console.error("Failed to create user", error);
+    throw createError({
+      statusCode: 500,
+      statusMessage: "Server error: " + error.message,
+    });
   }
 });
